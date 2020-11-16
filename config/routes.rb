0e8a0600c1873a81f5a716935784cf2669a50df2
Aung Maw <aunghmaw@hotmail.com>
2020-11-16 17:33:01 -0500

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'public/index'
   get 'public/show'
   resources :people do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_222551) do
+ActiveRecord::Schema.define(version: 2020_11_16_223211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_11_10_222551) do
     t.text "ssn_ciphertext"
     t.text "first_name_ciphertext"
     t.text "last_name_ciphertext"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
