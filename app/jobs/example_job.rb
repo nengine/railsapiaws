446c06670e5f348b52cd07483434b6adbd6c3829
Aung Maw <aunghmaw@hotmail.com>
2021-02-06 07:50:34 -0500

    replace first and lastname

--- app/jobs/example_job.rb
+++ app/jobs/example_job.rb
@@ -2,6 +2,6 @@ class ExampleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    User.create(first_name: args[0], last_name: args[1])
+    User.create(auth0_id: args[0], country_code: args[1])
   end
 end
